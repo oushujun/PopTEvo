c3da48b731db2f2fc8d50aff7e76d676cc53c5c2
Shujun Ou <shujun.ou.1@gmail.com>
2022-01-18 17:56:40 -0600

    reorder things

--- crossover/sam2bed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ml samtools
-ml bedtools2
-sam=$1
-samtools view -b -o ${sam%.*}.bam ${sam}
-samtools sort -o ${sam%.*}_sorted.bam ${sam%.*}.bam
-bedtools bamtobed -i ${sam%.*}_sorted.bam > ${sam%.*}.bed
